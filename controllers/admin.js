70e802300114b86ef72346ed601a2c710a45fcf2
Mykhailo Kazakov <mykhailo_kazakov@epam.com>
2022-02-04 21:21:29 +0200

    Completed product delete async request implementation

--- controllers/admin.js
+++ controllers/admin.js
@@ -198,8 +198,9 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
@@ -212,12 +213,13 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log('Deleted product!');
-      res.redirect('/admin/products');
+      res.status(200).json({
+        message: 'Success!',
+      });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-
-      return next(error);
+      res.status(500).json({
+        message: 'Deleting product failed.',
+      });
     });
 };
