a4d6933638fcf3cb4bf295a460c57076fc26b9b0
Mykhailo Kazakov <mykhailo_kazakov@epam.com>
2022-02-05 16:03:59 +0200

    Implemented Stripe payments

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,6 +5,12 @@ const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
+const product = require('../models/product');
+
+const stripeApiKey = require('../constants').stripeApiKey;
+const stripeSecretKey = require('../constants').stripeSecretKey;
+
+const stripe = require('stripe')(stripeSecretKey);
 
 const ITEMS_PER_PAGE = 2;
 
@@ -159,34 +165,6 @@ exports.getOrders = (req, res, next) => {
     });
 };
 
-exports.postOrder = (req, res, next) => {
-  return req.user
-    .populate('cart.items.productId')
-    .then((user) => {
-      const products = user.cart.items.map((product) => ({
-        quantity: product.quantity,
-        product: { ...product.productId._doc },
-      }));
-
-      const order = new Order({
-        user: {
-          email: req.user.email,
-          userId: req.user,
-        },
-        products,
-      });
-
-      return order.save();
-    })
-    .then(() => {
-      return req.user.clearCart();
-    })
-    .then(() => {
-      res.redirect('/orders');
-    })
-    .catch((err) => console.log(err));
-};
-
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
   Order.findById(orderId)
@@ -239,8 +217,75 @@ exports.getInvoice = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    pageTitle: 'Checkout',
-    path: '/checkout',
-  });
+  let products;
+  let total = 0;
+
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      products = user.cart.items;
+
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map((p) => {
+          return {
+            name: p.productId.title,
+            description: p.productId.description,
+            amount: p.productId.price * 100,
+            currency: 'usd',
+            quantity: p.quantity,
+          };
+        }),
+        success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+      });
+    })
+    .then(({ id }) => {
+      res.render('shop/checkout', {
+        products,
+        pageTitle: 'Checkout',
+        path: '/checkout',
+        totalSum: total,
+        stripeApiKey,
+        sessionId: id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  return req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      const products = user.cart.items.map((product) => ({
+        quantity: product.quantity,
+        product: { ...product.productId._doc },
+      }));
+
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products,
+      });
+
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch((err) => console.log(err));
 };
